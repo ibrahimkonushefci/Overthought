85b5c68cff5cb30f86c2d652b21edb300b8b5c74
Ibrahimi <ibrahimi@Ibrahimis-MacBook-Air.local>
2026-07-03 12:27:13 +0200

    iOS version changes

--- app.config.js
+++ app.config.js
@@ -24,7 +24,7 @@ const baseExpo = {
   name: 'Overthought',
   slug: 'overthought',
   scheme: 'overthought',
-  version: '0.1.0',
+  version: '1.0.1',
   orientation: 'portrait',
   userInterfaceStyle: 'light',
   icon: './assets/brand/app-icon.png',
