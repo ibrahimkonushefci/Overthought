edd0ce8cbfa3a0abd0d663302112d995fb5c9222
Ibrahimi <ibrahimi@Ibrahimis-MacBook-Air.local>
2026-09-19 15:31:23 +0200

    Bump iOS version to 1.0.5

--- app.config.js
+++ app.config.js
@@ -24,7 +24,7 @@ const baseExpo = {
   name: 'Overthought',
   slug: 'overthought',
   scheme: 'overthought',
-  version: '1.0.4',
+  version: '1.0.5',
   orientation: 'portrait',
   userInterfaceStyle: 'light',
   icon: './assets/brand/app-icon.png',
